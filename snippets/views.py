1031fcc7b4954098f82220db6ff8cdb9de6b0941
Uladzislau Vasilyuk <vlad007vladpedro@gmail.com>
2019-10-22 00:03:38 +0300

    learned hyperlinked API

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,9 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import mixins, generics, permissions
+from rest_framework import mixins, generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
 
@@ -64,3 +67,20 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
