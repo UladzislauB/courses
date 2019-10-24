eebd64bd101ee49be3d3707e6d973c9c11ecde33
Uladzislau Vasilyuk <vlad007vladpedro@gmail.com>
2019-10-24 13:17:00 +0300

    added Router instead of explicit URL conf

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,34 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import SnippetViewSet, api_root
+from snippets import views
+from rest_framework.routers import DefaultRouter
 
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-})
-user_list = SnippetViewSet.as_view({
-    'get': 'list'
-})
-user_detail = SnippetViewSet.as_view({
-    'get': 'retrieve'
-})
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'snippets', views.SnippetViewSet)
+
 urlpatterns = [
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>', user_detail, name='user-detail'),
-    path('', api_root),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight')
+    path('', include(router.urls))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -17,22 +17,14 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
-    queryset = Snippet.objecst.all()
+    queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-    @action(detail=True, renderer_classes=renderers.StaticHTMLRenderer)
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
         return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
