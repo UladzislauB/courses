529bd33173f9e7e19d8ea6c135a8f392fbd4cd97
Uladzislau Vasilyuk <vlad007vladpedro@gmail.com>
2019-12-01 18:03:20 +0300

    added base main page

---
+++ BethanyPieShop/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+
+            pieListViewModel.CurrentCategory = "Cheese cakes";
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ BethanyPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace BethanyPieShop.Models
+{
+    public class MockCategoryRepository: ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId = 1, CategoryName = "Fruit pies", Description = "All fruity pies"},
+                new Category{CategoryId = 2, CategoryName = "Cheese cakes", Description = "Cheese all the way"},
+                new Category{CategoryId = 3, CategoryName = "Seasonal pies", Description = "Get in the mood with a seasonal pie"}
+            };
+    }
+}
