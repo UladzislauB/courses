34f5864fb5645d96a1c631605a7734dae6abadc5
Uladzislau Vasiliuk <devuladblr@gmail.com>
2020-01-13 21:22:40 +0300

    initial commit for Django2.2_Bootcamp course

---
+++ Django_2.2_Bootcamp/wordcount/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/qwerty/__study__/courses/Django_2.2_Bootcamp/wordcount/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
